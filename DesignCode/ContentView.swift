b729a69cd731620944a9a01653d7296cd87a5fbd
魏英伦 <18574788512@163.com>
2020-06-01 14:58:40 +0800

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by 英伦 on 2020/6/1.
+//  Copyright © 2020 英伦. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
